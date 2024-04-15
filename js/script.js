e3d6e206f55baea8859a85107558cdd650f51ae9
Diegoswebdev <diegos.pg@hotmail.com>
2024-04-15 10:53:03 -0300

    terninei meu jogo em js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const clounds = document.querySelector('.clouds');
+
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump')
+
+    }, 500);
+}
+
+const loop = setInterval (()=> {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = +window.getComputedStyle(clounds).right.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${pipePosition}px`;
+
+        clounds.style.animation = 'none'
+        clounds.style.right = `${cloudsPosition}px`
+
+        mario.src = './imagens/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginleft = '50px';
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+
+
+document.addEventListener('keydown', jump);
